a7b20c17412ced7da50534ee1962c2d4547e99cc
wazzapr <52473610+wazzapr@users.noreply.github.com>
2026-09-25 22:22:12 +0200

    Start Liquidsoap with the radio server

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -6,6 +6,7 @@ import { randomUUID } from "node:crypto";
 import { addListener } from "./lib/radio-state";
 import { createServer } from "node:http";
 import { attachLiveRelay } from "./lib/live-relay";
+import { ensureLiquidsoap } from "./lib/liquidsoap";
 
 const rawPort = process.env["PORT"];
 
@@ -49,6 +50,7 @@ server.on("upgrade", (request, socket, head) => {
 });
 
 await ensureSeedData();
+await ensureLiquidsoap();
 attachLiveRelay(server);
 
 server.listen(port, () => {
